e2a9bef2ee1cf8558fbf985931e92f220992ef80
Catalina bucur <catalina.bucur@iis.utm.md>
2021-04-26 21:01:56 +0300

    Navigation

--- tehno-web/src/App.js
+++ tehno-web/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from "react-router-dom";
+import "./index.css";
+
+import { Tabs } from "antd";
+import "antd/dist/antd.css";
+const TabPane = Tabs.TabPane;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/tabs">Tabs</NavLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <div>Home</div>;
+            }}
+          />
+          <Route
+            path="/tabs/:tab?"
+            render={({ match, history }) => {
+              return (
+                <div className="card-container">
+                  <Switch>
+                    <Tabs
+                      type="card"
+                      defaultActiveKey={match.params.tab}
+                      onChange={key => {
+                        console.log(match);
+                        history.push(`/tabs/${key}`);
+                      }}
+                    >
+                      <TabPane tab="Tab Title 1" key="1">
+                        <p>Content of Tab Pane 1</p>
+                        <p>Content of Tab Pane 1</p>
+                        <p>Content of Tab Pane 1</p>
+                      </TabPane>
+                      <TabPane tab="Tab Title 2" key="2">
+                        <p>Content of Tab Pane 2</p>
+                        <p>Content of Tab Pane 2</p>
+                        <p>Content of Tab Pane 2</p>
+                      </TabPane>
+                      <TabPane tab="Tab Title 3" key="3">
+                        <p>Content of Tab Pane 3</p>
+                        <p>Content of Tab Pane 3</p>
+                        <p>Content of Tab Pane 3</p>
+                      </TabPane>
+                    </Tabs>
+                  </Switch>
+                </div>
+              );
+            }}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- tehno-web/src/index.js
+++ tehno-web/src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+const { Header, Content, Footer } = Layout;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <body>
+  <Router>
+    <Layout>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      
+    </Layout>
+
+    <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+    </Switch>
+    <Footer style={{ textAlign: 'center' }}> Catalina Bucur ©2021 UTM Project</Footer>
+  </Router>
+  </body>,
+  document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
